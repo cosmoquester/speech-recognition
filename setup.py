1b278a1c041cc3a913674ddfc9a8c09fef5c0fe7
박상준 <psj8252@naver.com>
2021-05-01 16:56:53 +0900

    feat: Add default dataset loader

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     description="Develope speech recognition models with tensorflow 2",
     python_requires=">=3.6",
-    install_requires=["tensorflow>=2"],
+    install_requires=["tensorflow>=2", "tensorflow-io", "tensorflow-text"],
     url="https://github.com/psj8252/speech-recognition.git",
     author="Park Sangjun",
     packages=find_packages(exclude=["tests"]),
--- speech_recognition/data.py
+++ speech_recognition/data.py
@@ -1,14 +1,44 @@
+from typing import Optional, Tuple
+
 import tensorflow as tf
+import tensorflow_io as tfio
+import tensorflow_text as text
 
 
-def get_dataset(dataset_file_path: str):
+def get_dataset(
+    dataset_file_path: str,
+    tokenizer: text.SentencepieceTokenizer,
+    resample: Optional[int] = None,
+) -> tf.data.Dataset:
     """
-    Read dataset file and construct tensorflow dataset
+    Load dataset from tsv file. The dataset file has to header.
+    The first column has audio file path and second column has recognized sentence.
 
-    :param dataset_file_path: dataset file path.
+    :param dataset_file_path: dataset file path
+    :param tokenizer: sentencepiece tokenizer
+    :param resample: resample rate (default no resample)
+    :return: tensorflow dataset
     """
-    # Replace the context of this function
-    dataset_x = tf.data.Dataset.range(10000).map(lambda x: x[tf.newaxis])
-    dataset_y = tf.data.Dataset.from_tensor_slices([0, 1] * 5000)
+    dataset = tf.data.experimental.CsvDataset(
+        dataset_file_path, [tf.string, tf.string], header=True, field_delim="\t", use_quote_delim=False
+    )
 
-    return tf.data.Dataset.zip((dataset_x, dataset_y))
+    def _load_example(audio_file_path: tf.Tensor, sentence: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
+        """
+        Load audio file and tokenize sentence.
+
+        :param audio_file_path: string tensor that is audio file path
+        :param sentence: string tensor that is recognized sentence from audio
+        :return: audio and sentence tensor
+        """
+        # audio: [TimeStep, NumChannel]
+        audio, sample_rate = tf.audio.decode_wav(tf.io.read_file(audio_file_path))
+        # tokens: [NumTokens]
+        tokens = tokenizer.tokenize(sentence)
+
+        # Resample
+        if resample is not None:
+            audio = tfio.audio.resample(audio, sample_rate, resample, name="resampling")
+        return audio, tokens
+
+    return dataset.map(_load_example, num_parallel_calls=tf.data.experimental.AUTOTUNE)
