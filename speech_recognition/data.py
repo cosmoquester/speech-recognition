572c41ebe14a5bc7bb6d6a45d3604ebd15dd6321
박상준 <psj8252@naver.com>
2021-05-01 17:43:39 +0900

    feat: Add function to make spectogram

--- speech_recognition/data.py
+++ speech_recognition/data.py
@@ -8,6 +8,7 @@ import tensorflow_text as text
 def get_dataset(
     dataset_file_path: str,
     tokenizer: text.SentencepieceTokenizer,
+    desired_channels: int = -1,
     resample: Optional[int] = None,
 ) -> tf.data.Dataset:
     """
@@ -16,8 +17,9 @@ def get_dataset(
 
     :param dataset_file_path: dataset file path
     :param tokenizer: sentencepiece tokenizer
+    :param desired_channels: number of sample channels wanted. default is auto
     :param resample: resample rate (default no resample)
-    :return: tensorflow dataset
+    :return: PCM audio and tokenized sentence dataset
     """
     dataset = tf.data.experimental.CsvDataset(
         dataset_file_path, [tf.string, tf.string], header=True, field_delim="\t", use_quote_delim=False
@@ -32,7 +34,7 @@ def get_dataset(
         :return: audio and sentence tensor
         """
         # audio: [TimeStep, NumChannel]
-        audio, sample_rate = tf.audio.decode_wav(tf.io.read_file(audio_file_path))
+        audio, sample_rate = tf.audio.decode_wav(tf.io.read_file(audio_file_path), desired_channels)
         # tokens: [NumTokens]
         tokens = tokenizer.tokenize(sentence)
 
@@ -42,3 +44,25 @@ def get_dataset(
         return audio, tokens
 
     return dataset.map(_load_example, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+
+@tf.function
+def make_spectogram(audio: tf.Tensor, frame_length: int, frame_step: int, fft_length=None) -> tf.Tensor:
+    """
+    Make spectogram from PCM audio dataset.
+
+    :param audio: pcm format audio tensor shaped [TimeStep, NumChannel]
+    :param frame_length: window length in samples
+    :param frame_step: number of samples to step
+    :param fft_length: size of the FFT to apply. By default, uses the smallest power of 2 enclosing frame_length
+    :return: spectogram audio tensor shaped [NumFrame, NumChannel, NumFFTUniqueBins]
+    """
+    # Shape: [NumChannel, TimeStep]
+    audio = tf.transpose(audio)
+    # Shape: [NumChannel, NumFrame, NumFFTUniqueBins]
+    spectogram = tf.signal.stft(audio, frame_length, frame_step, fft_length)
+    spectogram = tf.abs(spectogram)
+
+    # Shape: [NumFrame, NumChannel, NumFFTUniqueBins]
+    spectogram = tf.transpose(spectogram, [1, 0, 2])
+    return spectogram
