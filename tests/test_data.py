f43a467194afeeaf2d9bf62aae985610f441c43c
박상준 <psj8252@naver.com>
2021-05-02 17:20:21 +0900

    test: Add test for log mel spectrogram

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,7 +3,7 @@ import os
 import pytest
 import tensorflow as tf
 
-from speech_recognition.data import get_dataset, make_spectogram
+from speech_recognition.data import get_dataset, make_log_mel_spectrogram, make_spectrogram
 
 DATA_DIR = os.path.dirname(__file__)
 DATASET_PATH = os.path.join(DATA_DIR, "data", "dataset.tsv")
@@ -42,13 +42,40 @@ def test_get_dataset(dataset):
         (512, 512, 256),
     ],
 )
-def test_make_spectogram(dataset, frame_length, frame_step, fft_length):
+def test_make_spectrogram(dataset, frame_length, frame_step, fft_length):
     audio_timestep = tf.shape(next(iter(dataset))[0])[0]
-    dataset = dataset.map(lambda audio, _: make_spectogram(audio, frame_length, frame_step, fft_length))
+    dataset = dataset.map(lambda audio, _: make_spectrogram(audio, frame_length, frame_step, fft_length))
     audio_sample = next(iter(dataset))
 
     if fft_length is None:
         fft_length = frame_length
     tf.debugging.assert_equal(
         tf.shape(audio_sample), [(audio_timestep - frame_length + frame_step) // frame_step, 1, fft_length // 2 + 1]
+    )
+
+
+@pytest.mark.parametrize(
+    "sample_rate,frame_length,frame_step,fft_length,num_mel_bins,lower_edge_hertz,upper_edge_hertz",
+    [
+        (22050, 1024, 1024, 1024, 80, 10, 10000),
+        (16000, 128, 64, 256, 123, 12, 88),
+        (32000, 128, 80, 128, 321, 32, 16000),
+        (44100, 512, 512, 256, 333, 333, 3333),
+    ],
+)
+def test_make_log_mel_spectrogram(
+    dataset, sample_rate, frame_length, frame_step, fft_length, num_mel_bins, lower_edge_hertz, upper_edge_hertz
+):
+    audio_timestep = tf.shape(next(iter(dataset))[0])[0]
+    dataset = dataset.map(
+        lambda audio, _: make_log_mel_spectrogram(
+            audio, sample_rate, frame_length, frame_step, fft_length, num_mel_bins, lower_edge_hertz, upper_edge_hertz
+        )
+    )
+    audio_sample = next(iter(dataset))
+
+    if fft_length is None:
+        fft_length = frame_length
+    tf.debugging.assert_equal(
+        tf.shape(audio_sample), [(audio_timestep - frame_length + frame_step) // frame_step, 1, num_mel_bins]
     )
