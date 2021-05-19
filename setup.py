e893fba5525cca17f8f48b5ccca43dd816e80bd3
psj8252 <psj8252@naver.com>
2021-05-19 13:12:49 +0900

    build: Prevent tensorflow 2.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     description="Develope speech recognition models with tensorflow 2",
     python_requires=">=3.6",
-    install_requires=["tensorflow>=2", "tensorflow-io", "tensorflow-text", "tqdm", "omegaconf"],
+    install_requires=["tensorflow>=2,<2.5", "tensorflow-io", "tensorflow-text", "tqdm", "omegaconf"],
     url="https://github.com/psj8252/speech-recognition.git",
     author="Park Sangjun",
     packages=find_packages(exclude=["tests"]),
